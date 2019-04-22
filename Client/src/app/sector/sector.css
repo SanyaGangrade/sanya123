24c73bf516559cc43d754109edb42da231029d82
SanyaGangrade <49864615+SanyaGangrade@users.noreply.github.com>
2019-04-22 17:02:28 +0530

    Create sector.css

---
+++ Client/src/app/sector/sector.css
@@ -0,0 +1,35 @@
+table, th, td 
+{
+    margin: 10px 0;
+    border: solid 1px #333;
+    padding: 2px 4px;
+    font: 15px Verdana;
+}
+th {
+    font-weight:bold;
+}
+button {
+  /*  background-color:rgb(105, 123, 228);
+    color: white;
+    padding: 20px 20px;
+    margin: 6px 6px;
+    border: none;
+    cursor: pointer;
+    width: auto;
+    float: left;*/
+ 
+   
+    text-transform: none;
+    padding:10px;
+    padding-left:15px;
+    padding-right:15px;
+     font-size: 20px;
+     width:120px;
+     color: #000;
+     background-color: rgb(94, 94, 221)
+
+/* color: white;*/
+  
+   
+   
+  }
